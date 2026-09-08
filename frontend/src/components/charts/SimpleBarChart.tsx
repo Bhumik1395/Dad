88e778e35955eafada272fff04f317016c8a0109
BhumikJoshi <147263081+Bhumik1395@users.noreply.github.com>
2026-09-08 22:53:55 +0530

    Update SimpleBarChart.tsx

--- frontend/src/components/charts/SimpleBarChart.tsx
+++ frontend/src/components/charts/SimpleBarChart.tsx
@@ -9,7 +9,7 @@ export function SimpleBarChart({
 }) {
     const option = {
         color: ["#0F6E56"],
-        grid: { left: 35, right: 10, top: 25, bottom: 50 },
+        grid: { left: 35, right: 10, top: 25, bottom: 40 },
         xAxis: {
             type: "category",
             data: data.map((d) => d[xKey]),
